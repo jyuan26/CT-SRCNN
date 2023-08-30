bdd15971c40f6483faa8ed27635f980e1feaae19
fried <friedepigs@gmail.com>
2023-08-30 22:18:16 +0000

    calculate PSNR,MSE,SSIM for all files in input dir

--- test.py
+++ test.py
@@ -4,10 +4,13 @@ import torch
 import torch.backends.cudnn as cudnn
 import numpy as np
 import PIL.Image as pil_image
+import os
 
 from models import SRCNN
-from utils import convert_rgb_to_ycbcr, convert_ycbcr_to_rgb, calc_psnr
+from utils import convert_rgb_to_ycbcr, convert_ycbcr_to_rgb, calc_psnr, calc_mse, compute_ssim
 
+def get_list(path, ext):
+    return [os.path.join(path, f) for f in os.listdir(path) if f.endswith(ext)]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -30,32 +33,59 @@ if __name__ == '__main__':
 
     model.eval()
 
-    image = pil_image.open(args.image_file).convert('RGB')
+    max_test_item = 2000
+    filelist = get_list(args.image_file, ext="png")
+    filelist = filelist[:max_test_item]
+    psnr_list = np.zeros(len(filelist))
+    mse_list = np.zeros(len(filelist))
+    ssim_list = np.zeros(len(filelist))
+    i = 0
 
-    image_width = (image.width // args.scale) * args.scale
-    image_height = (image.height // args.scale) * args.scale
-    image = image.resize((image_width, image_height), resample=pil_image.BICUBIC)
-    image = image.resize((image.width // args.scale, image.height // args.scale), resample=pil_image.BICUBIC)
-    image = image.resize((image.width * args.scale, image.height * args.scale), resample=pil_image.BICUBIC)
-    image.save(args.image_file.replace('.', '_bicubic_x{}.'.format(args.scale)))
+    for imname in filelist:
 
-    image = np.array(image).astype(np.float32)
-    ycbcr = convert_rgb_to_ycbcr(image)
+        image = pil_image.open(imname).convert('RGB')
 
-    y = ycbcr[..., 0]
-    y /= 255.
-    y = torch.from_numpy(y).to(device)
-    y = y.unsqueeze(0).unsqueeze(0)
+        image_width = (image.width // args.scale) * args.scale
+        image_height = (image.height // args.scale) * args.scale
+        image = image.resize((image_width, image_height), resample=pil_image.BICUBIC)
+        image = image.resize((image.width // args.scale, image.height // args.scale), resample=pil_image.BICUBIC)
+        image = image.resize((image.width * args.scale, image.height * args.scale), resample=pil_image.BICUBIC)
+        bicublic = imname.replace('2020','output/2020')
+        image.save(bicublic.replace('.', '_bicubic_x{}.'.format(args.scale)))
 
-    with torch.no_grad():
-        preds = model(y).clamp(0.0, 1.0)
+        image = np.array(image).astype(np.float32)
+        ycbcr = convert_rgb_to_ycbcr(image)
 
-    psnr = calc_psnr(y, preds)
-    print('PSNR: {:.2f}'.format(psnr))
+        y = ycbcr[..., 0]
+        y /= 255.
+        y = torch.from_numpy(y).to(device)
+        y = y.unsqueeze(0).unsqueeze(0)
 
-    preds = preds.mul(255.0).cpu().numpy().squeeze(0).squeeze(0)
+        with torch.no_grad():
+            preds = model(y).clamp(0.0, 1.0)
 
-    output = np.array([preds, ycbcr[..., 1], ycbcr[..., 2]]).transpose([1, 2, 0])
-    output = np.clip(convert_ycbcr_to_rgb(output), 0.0, 255.0).astype(np.uint8)
-    output = pil_image.fromarray(output)
-    output.save(args.image_file.replace('.', '_srcnn_x{}.'.format(args.scale)))
+        psnr_list[i] = calc_psnr(y, preds)
+        #mse_list[i] = calc_mse(y, preds)
+        ssim_y = y.numpy().squeeze(0).squeeze(0)
+        ssim_preds = preds.numpy().squeeze(0).squeeze(0)
+        ssim_list[i] = compute_ssim(ssim_y, ssim_preds)
+        #psnr_list[i] = calc_psnr(ssim_y, ssim_preds)
+        mse_list[i] = calc_mse(y, ssim_preds)
+
+        print("i="+str(i)+',PSNR: {:.9f}'.format(psnr_list[i]) +
+              ",mse: {:.9f}".format(mse_list[i]) + ",ssie: {:.9f}".format(ssim_list[i]))
+
+
+        preds = preds.mul(255.0).cpu().numpy().squeeze(0).squeeze(0)
+
+        output = np.array([preds, ycbcr[..., 1], ycbcr[..., 2]]).transpose([1, 2, 0])
+        output = np.clip(convert_ycbcr_to_rgb(output), 0.0, 255.0).astype(np.uint8)
+        output = pil_image.fromarray(output)
+        #output_dir = imname + "/output/"
+        imname = imname.replace('2020','output/2020')
+        output.save(imname.replace('.', '_srcnn_x{}.'.format(args.scale)))
+        i = i + 1
+    psnr = np.mean(psnr_list)
+    print("Mean PSNR: " + str(psnr))
+    print("Mean MSE: " + str(np.mean(mse_list)))
+    print("Mean SSIM: " + str(np.mean(ssim_list)))
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+from skimage.metrics import structural_similarity as ssim
 
 
 def convert_rgb_to_y(img):
@@ -49,6 +50,19 @@ def convert_ycbcr_to_rgb(img):
 
 def calc_psnr(img1, img2):
     return 10. * torch.log10(1. / torch.mean((img1 - img2) ** 2))
+
+
+def calc_mse(img1, img2):
+    #return ((img1 - img2) ** 2).mean()
+    return np.square(img1 - img2).mean()
+    #return torch.mean((img1 - img2) ** 2)
+
+
+def compute_ssim(im1, im2):
+    isRGB = len(im1.shape) == 3 and im1.shape[-1] == 3
+    s = ssim(im1, im2, K1=0.01, K2=0.03, gaussian_weights=True, sigma=1.5, use_sample_covariance=False,
+             multichannel=isRGB, data_range=255.0)
+    return s
 
 
 class AverageMeter(object):
